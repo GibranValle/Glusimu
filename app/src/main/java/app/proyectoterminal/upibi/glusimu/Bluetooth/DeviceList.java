35b5b0805f933344a1b1f7bbfe586672c45b1b32
GibranValle <gibranvalle@hotmail.com>
2016-05-15 05:31:41 -0500

    Initial commit

---
+++ app/src/main/java/app/proyectoterminal/upibi/glusimu/Bluetooth/DeviceList.java
@@ -0,0 +1,215 @@
+/*
+ * Copyright 2014 Akexorcist
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package app.proyectoterminal.upibi.glusimu.Bluetooth;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.Set;
+
+import app.proyectoterminal.upibi.glusimo.R;
+
+@SuppressLint("NewApi")
+public class DeviceList extends Activity {
+    // Debugging
+    private static final String TAG = "DeviceList";
+    private static final boolean D = true;
+
+    // Member fields
+    private BluetoothAdapter BTadaptador;
+    private Button boton;
+    private ArrayAdapter<String> arrayDevicesVinculados;
+    private ArrayAdapter<String> arrayDevicesEncontrados;
+    private IntentFilter filtro;
+    private Set<BluetoothDevice> pairedDevices;
+    private ProgressBar bolita;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.i(TAG,"DeviceList creada");
+        
+        // Setup the window
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.bluetooth_lista_devices);
+
+        // Set result CANCELED in case the user backs out
+        setResult(Activity.RESULT_CANCELED);
+
+        // RECUPERAR LOS XML DE LOS ELEMENTOS
+        boton = (Button) findViewById(R.id.boton_lista_devices);
+        bolita = (ProgressBar)findViewById(R.id.bolita);
+
+        // Recuperar String de boton,
+        String stringBuscar = getIntent().getStringExtra("scan_for_devices");
+        if(stringBuscar == null)
+        	stringBuscar = "BUSCAR DOSIFICADOR";
+        // Cargar el string en el boton
+        boton.setText(stringBuscar);
+        // Habilitar el discovery si no se encontraron dosificador
+        boton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                doDiscovery();
+            }
+        });
+
+        // inicializa los arreglos para devices vinculados y encontrados
+        arrayDevicesVinculados = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        arrayDevicesEncontrados = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+
+        // agrega los resources desde xml
+        ListView pairedListView = (ListView) findViewById(R.id.lista_encontrados);
+        ListView newDevicesListView = (ListView) findViewById(R.id.lista_nuevos);
+
+        // setea los adaptadores necesarios
+        pairedListView.setAdapter(arrayDevicesVinculados);
+        newDevicesListView.setAdapter(arrayDevicesEncontrados);
+
+        // agrega el responsable del listener
+        pairedListView.setOnItemClickListener(mDeviceClickListener);
+        newDevicesListView.setOnItemClickListener(mDeviceClickListener);
+
+        // Crea IntentFilter y registra los broadcast para cada accion respectivamente
+        filtro = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        this.registerReceiver(mReceiver, filtro);
+        filtro = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        this.registerReceiver(mReceiver, filtro);
+
+        // abrir el adaptador local
+        BTadaptador = BluetoothAdapter.getDefaultAdapter();
+        //carga los dispositivos vinculados
+        pairedDevices = BTadaptador.getBondedDevices();
+
+        // si encuentra dispositivos vinculados, los agrega al array
+        if (pairedDevices.size() > 0) {
+            findViewById(R.id.titulo).setVisibility(View.VISIBLE); //hace visible el array
+            for (BluetoothDevice device : pairedDevices) {
+                arrayDevicesVinculados.add(device.getName() + "\n" + device.getAddress()); //agregando al array
+            }
+        } else {
+            // si no existen dispositivos vinculados, carga un string
+            String noDevices = getResources().getText(R.string.no_vinculados).toString();
+            arrayDevicesVinculados.add(noDevices);
+        }
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        // Make sure we're not doing discovery anymore
+        if (BTadaptador != null) {
+            BTadaptador.cancelDiscovery();
+        }
+
+        // Unregister broadcast listeners
+        this.unregisterReceiver(mReceiver);
+        this.finish();
+    }
+
+    // Start device discover with the BluetoothAdapter
+	private void doDiscovery()
+    {
+        if (D) Log.d(TAG, "doDiscovery()");
+
+        // carga el progress bar
+        bolita.setVisibility(View.VISIBLE);
+
+        // cancelar el discovery si se esta efectuando
+        if (BTadaptador.isDiscovering()) {
+            BTadaptador.cancelDiscovery();
+        }
+
+        // empieza el discovery
+        BTadaptador.startDiscovery();
+
+    }
+
+    //crea el listener para todos los listviews
+    private OnItemClickListener mDeviceClickListener = new OnItemClickListener() {
+        public void onItemClick(AdapterView<?> av, View v, int arg2, long arg3) {
+
+            Log.i(TAG,"ListView Click");
+
+            // Cancel discovery because it's costly and we're about to connect
+            if(BTadaptador.isDiscovering())
+                BTadaptador.cancelDiscovery();
+            // recupera la direccion MAC del dispositivo, son los ultimos 17 caracteres del view
+            String info = ((TextView) v).getText().toString();
+            String address = info.substring(info.length() - 17);
+            String name = info.substring(0,info.length() - 18);
+            Log.i(TAG, name);
+
+            // crea el intent y guarda la direccion MAC
+            Intent intent = new Intent();
+            intent.putExtra(BluetoothState.EXTRA_DEVICE_ADDRESS, address);
+            intent.putExtra(BluetoothState.EXTRA_DEVICE_NAME, name);
+
+            // Setea el result y termina la activity
+            setResult(Activity.RESULT_OK, intent);
+            Log.i(TAG,"Activity Result Ok");
+            finish();
+        }
+    };
+
+    // El broadcast que encuentra los dispositivos y los agrega al array cuando termina el discovery
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent)
+        {
+            String action = intent.getAction();
+            // carga la vista de dispositivos encontrados
+            findViewById(R.id.subtitulo).setVisibility(View.VISIBLE);
+            // Si se encuentra un device
+            if (BluetoothDevice.ACTION_FOUND.equals(action))
+            {
+                //recupera el objeto device del intent
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // saltar si ya esta vinculado
+                if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
+                    arrayDevicesEncontrados.add(device.getName() + "\n" + device.getAddress());
+                }
+                //cuando termina el discovery, cambia la vista
+            }
+            else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action))
+            {
+                bolita.setVisibility(View.GONE);
+                if (arrayDevicesEncontrados.getCount() == 0) {
+                    String noDevices = getResources().getText(R.string.no_encontrados).toString();
+                    arrayDevicesEncontrados.add(noDevices);
+                }
+            }
+        }
+    };
+
+}
---
+++ app/src/main/java/app/proyectoterminal/upibi/glusimu/Interfaz.java
@@ -0,0 +1,290 @@
+package app.proyectoterminal.upibi.glusimu;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.v4.view.MotionEventCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import app.proyectoterminal.upibi.glusimu.Bluetooth.BluetoothSPP;
+import app.proyectoterminal.upibi.glusimu.Bluetooth.BluetoothState;
+
+public class Interfaz extends AppCompatActivity implements View.OnTouchListener {
+
+    String TAG = "Interfaz";
+
+    // VARIABLES PARA GRAFICAR
+    ImageView espacio;
+    Bitmap bitmap;
+    Canvas canvas;
+    Paint paint;
+    int alto, ancho;
+    float x, y;
+    SharedPreferences respaldo;
+
+    // Variable bluetooth
+    BluetoothSPP bt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interfaz);
+
+        espacio = (ImageView) findViewById(R.id.espacio);
+        espacio.setOnTouchListener(this);
+
+        //  ----------------- BLUETOOTH -----------------------------------------//
+        bt = new BluetoothSPP(this);
+        if(!bt.isBluetoothAvailable())
+        {
+            Toast.makeText(getApplicationContext()
+                    , "Bluetooth is not available"
+                    , Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+
+        bt.setOnDataReceivedListener(new BluetoothSPP.OnDataReceivedListener() {
+            public void onDataReceived(byte[] data, String message)
+            {
+                String dato;
+                int valor;
+                //textRead.append(message + "\n");
+                if(message.startsWith("V"))
+                {
+                    Log.d(TAG,"mensaje recibido: "+message+" largo: "+message.length());
+                    dato = message.substring(1,message.length());
+                    valor = Integer.parseInt(dato);
+                    Log.d(TAG,"conversion correcta a entero: "+valor);
+                }
+
+
+            }
+        });
+
+
+        bt.setBluetoothConnectionListener(new BluetoothSPP.BluetoothConnectionListener()
+        {
+            public void onDeviceDisconnected()
+            {
+                // CAMBIAR TEXTO DE CONSOLA CONEXION
+
+            }
+
+            public void onDeviceConnectionFailed()
+            {
+                // CAMBIAR TEXTO DE CONSOLA CONEXION
+            }
+
+            public void onDeviceConnected(String name, String address)
+            {
+
+            }
+        });
+
+        bt.setBluetoothStateListener(new BluetoothSPP.BluetoothStateListener()
+        {
+            public void onServiceStateChanged(int state)
+            {
+                if(state == BluetoothState.STATE_CONNECTED)
+                {
+
+                }
+                else if(state == BluetoothState.STATE_CONNECTING)
+                {
+
+                }
+                else if(state == BluetoothState.STATE_LISTEN)
+                {
+                    // Do something when device is waiting for connection
+                }
+                else if(state == BluetoothState.STATE_NONE)
+                {
+                    // Do something when device don't have any connection
+                }
+            }
+        });
+        //  ----------------- BLUETOOTH -----------------------------------------//
+
+        // medidor
+
+        ViewTreeObserver vto = espacio.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener()
+        {
+            @Override
+            public void onGlobalLayout()
+            {
+                // CONSIGUE LOS DATOS DE ANCHO Y ALTO HASTA ESTE PUNTO
+                alto = espacio.getHeight();
+                ancho = espacio.getWidth();
+                Log.i(TAG,"alto: "+alto+" ancho: "+ancho);
+
+                ViewTreeObserver obs = espacio.getViewTreeObserver();
+                obs.removeGlobalOnLayoutListener(this);
+                bitmap = Bitmap.createBitmap(ancho, alto, Bitmap.Config.ARGB_8888);
+                Bitmap b= BitmapFactory.decodeResource(getResources(), R.drawable.contorno_ps);
+                canvas = new Canvas(bitmap);
+                paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+                canvas.drawBitmap(b,0,0,paint);
+                espacio.setImageBitmap(b);
+            }
+        });
+    }
+    protected void onResume()
+    {
+        super.onResume();
+        Log.v(TAG, "On Resume");
+
+        if (!bt.isBluetoothEnabled()) // no habilitado
+        {
+            // para arrancar el bluetooth sin pedir permiso:
+            bt.enable();
+
+            // PARA PEDIR PERMISO PARA REALIZAR LA CONEXION:
+            /*
+            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(intent, BluetoothState.REQUEST_ENABLE_BT);
+            */
+        }
+        else
+        {
+            if(!bt.isServiceAvailable())
+            {
+                bt.setupService();
+                bt.startService(BluetoothState.DEVICE_OTHER);
+            }
+        }
+    }
+    protected void onDestroy()
+    {
+        super.onDestroy();
+
+        if (bt.isBluetoothEnabled()) // no habilitado
+        {
+            bt.stopService();
+            bt.disconnect();
+            bt.disable();
+        }
+    }
+
+
+    /** -------------------- METODOS DE MENU --------------------------------*/
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_interfaz, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.menu_interfaz)
+        {
+            vibrar(100);
+            // LA POSICION 3 ES CONFIG
+            // ESTE METODO SE DESPLAZA AL FRAGMENT ELEGIDO
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    /** -------------------- METODOS DE MENU --------------------------------*/
+
+    void vibrar(int ms)
+    {
+        Vibrator vibrador = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        vibrador.vibrate(ms);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event)
+    {
+        int action = MotionEventCompat.getActionMasked(event);
+        x = event.getX();
+        y = event.getY();
+        float pcX, pcY;
+        switch (action)
+        {
+            case (MotionEvent.ACTION_DOWN):
+                //Log.d(TAG, "La accion ha sido APRIETA x:"+x +" y: "+y);
+                return true;
+            case (MotionEvent.ACTION_MOVE):
+                //Log.d(TAG, "La acción ha sido APRETANDO x:"+x +" y: "+y);
+                return true;
+            case (MotionEvent.ACTION_UP):
+                vibrar(100);
+                pcX = x/ancho*100;
+                pcY = y/alto*100;
+                Log.d(TAG," CLICK x: "+pcX+" y: "+pcY);
+                if (  (pcX > 40 && pcX <60)  && (pcY > 3 && pcY < 12)  )
+                {
+                    //Toast.makeText(Interfaz.this, "Click al cerebro", Toast.LENGTH_SHORT).show();
+                }
+
+                else if (  (pcX > 44 && pcX <59)  && (pcY > 44 && pcY < 51)  )
+                {
+                    //Toast.makeText(Interfaz.this, "Click al corazón", Toast.LENGTH_SHORT).show();
+                }
+
+                else if ( ( (pcX > 32 && pcX <44) || (pcX > 60 && pcX <70) ) && (pcY > 33 && pcY < 50)  )
+                {
+                    //Toast.makeText(Interfaz.this, "Click al pulmon", Toast.LENGTH_SHORT).show();
+                }
+
+                else if ( ( (pcX > 10 && pcX <20) || (pcX > 80 && pcX <90) ) && (pcY > 45 && pcY < 60)  )
+                {
+                    //Toast.makeText(Interfaz.this, "Click al musculo", Toast.LENGTH_SHORT).show();
+                }
+
+                else if (  (pcX > 35 && pcX <60)  && (pcY > 55 && pcY < 65)  )
+                {
+                    //Toast.makeText(Interfaz.this, "Click al higado", Toast.LENGTH_SHORT).show();
+                }
+
+                else if ( ( (pcX > 31 && pcX <36) || (pcX > 65 && pcX <70) ) && (pcY > 70 && pcY < 78)  )
+                {
+                    //Toast.makeText(Interfaz.this, "Click al riñones", Toast.LENGTH_SHORT).show();
+                }
+
+                else if (  (pcX > 45 && pcX <60)  && (pcY > 65 && pcY < 73)  )
+                {
+                    //Toast.makeText(Interfaz.this, "Click al pancreas", Toast.LENGTH_SHORT).show();
+                }
+
+                else if (  (pcX > 35 && pcX <65)  && (pcY > 80 && pcY < 90)  )
+                {
+                    //Toast.makeText(Interfaz.this, "Click al intestino", Toast.LENGTH_SHORT).show();
+                }
+
+
+
+                return true;
+            case (MotionEvent.ACTION_CANCEL):
+                Log.d(TAG, "La accion ha sido CANCELAR x:"+x +" y: "+y);
+                return true;
+            case (MotionEvent.ACTION_OUTSIDE):
+                Log.d(TAG,
+                        "La accion ha sido fuera del elemento de la pantalla x:"+x +" y: "+y);
+                return true;
+            default:
+                return true;
+        }
+    }
+}
